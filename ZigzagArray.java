f0ba2e4b08a57031492b4b54419dc0050260a6d3
Divya Kapoor <38455002+divyakapoor23@users.noreply.github.com>
2023-04-30 21:29:34 -0700

    Create ZigzagArray.java

---
+++ ZigzagArray.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+public class ZigzagArray {
+    public static void main(String[] args) {
+        int[] arr = {4, 3, 7, 8, 6, 2, 1};
+
+    // Sort the array in ascending order
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        // Print the sorted array
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+
+
+        // Swap adjacent elements starting from the second element
+        for (int i = 1; i < arr.length - 1; i += 2) {
+            int temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
+
